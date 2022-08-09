bc2c41b885d05e552bfad1a59ed28d06932caf46
home <home@MSK-NB-POZ2.ad.kdl-test.ru>
2022-08-09 23:15:27 +0300

    Add TicketByFlightTimeComparator

--- src/main/java/TicketManager.java
+++ src/main/java/TicketManager.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class TicketManager {
     private TicketRepository repository = new TicketRepository();
@@ -11,7 +12,7 @@ public class TicketManager {
         repository.save(ticket);
     }
 
-    public Ticket[] findAll(String from, String to) {
+    public Ticket[] findAll(String from, String to, Comparator<Ticket> comparator) {
         Ticket[] result = new Ticket[0];
         for (Ticket tckt : repository.getItems()) {
             if (tckt.takeOff == from && tckt.landing == to) {
@@ -23,7 +24,7 @@ public class TicketManager {
                 result = tmp;
             }
         }
-        Arrays.sort(result);
+        Arrays.sort(result, comparator);
         return result;
     }
 }
